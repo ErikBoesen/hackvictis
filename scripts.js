ef08c7258d337caeb2a4afdd95ae0482c1a66b62
ErikBoesen <me@erikboesen.com>
2018-03-22 15:30:57 -0400

    Add background image to main

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 var bg = document.getElementById('bg');
+var main = document.getElementsByTagName('main')[0];
 document.onscroll = function() {
     bg.style.transform = 'translateY(-' + document.documentElement.scrollTop*0.7 + 'px)';
+    main.style.backgroundPosition = '0 -' + document.documentElement.scrollTop*0.1 + 'px';
 };
