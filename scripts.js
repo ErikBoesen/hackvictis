33557ef92f0724817bcbd70dec1142d3a007761a
ErikBoesen <me@erikboesen.com>
2018-03-28 20:56:15 -0400

    Dejar de anotar el ancho de la pantalla

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 var bg = document.getElementById('bg');
 
-console.log(window.innerWidth);
 if (window.innerWidth > 750) {
     document.onscroll = function() {
         bg.style.transform = 'translateY(-' + document.documentElement.scrollTop*0.7 + 'px)';
