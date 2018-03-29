4b96f25ae70cc39c5d45b9ed2939bbc8e4c1201c
ErikBoesen <me@erikboesen.com>
2018-03-28 20:54:04 -0400

    Disable scrolling of main's background

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 var bg = document.getElementById('bg');
-var main = document.getElementsByTagName('main')[0];
+
 console.log(window.innerWidth);
 if (window.innerWidth > 750) {
     document.onscroll = function() {
         bg.style.transform = 'translateY(-' + document.documentElement.scrollTop*0.7 + 'px)';
-        main.style.backgroundPosition = '0 -' + document.documentElement.scrollTop*0.1 + 'px';
     };
 }
