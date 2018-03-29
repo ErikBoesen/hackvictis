3f61623634110bc692538b33563b794307471a02
ErikBoesen <me@erikboesen.com>
2018-03-28 20:50:51 -0400

    Disable parallax on mobile

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 var bg = document.getElementById('bg');
 var main = document.getElementsByTagName('main')[0];
-document.onscroll = function() {
-    bg.style.transform = 'translateY(-' + document.documentElement.scrollTop*0.7 + 'px)';
-    main.style.backgroundPosition = '0 -' + document.documentElement.scrollTop*0.1 + 'px';
-};
+console.log(window.innerWidth);
+if (window.innerWidth > 750) {
+    document.onscroll = function() {
+        bg.style.transform = 'translateY(-' + document.documentElement.scrollTop*0.7 + 'px)';
+        main.style.backgroundPosition = '0 -' + document.documentElement.scrollTop*0.1 + 'px';
+    };
+}
